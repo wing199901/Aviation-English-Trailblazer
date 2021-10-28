42330186df043f469f9dc0910d5e4f94439dda61
Steven Siu <wing199901@gmail.com>
2021-10-28 15:37:47 +0800

    Initial Commit

---
+++ Aviation English TrailblazerTests/Aviation_English_TrailblazerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Aviation_English_TrailblazerTests.swift
+//  Aviation English TrailblazerTests
+//
+//  Created by Steven Siu  on 28/10/2021.
+//
+
+import XCTest
+@testable import Aviation_English_Trailblazer
+
+class Aviation_English_TrailblazerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
